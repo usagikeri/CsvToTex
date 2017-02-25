d0e3b6b7e8f710aff663ecac46f9cb438c4497ba
Usagikeri <candidandoras@gmail.com>
2017-02-25 23:41:21 +0900

    First commit

---
+++ csvToTex.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+#　This program was created　by Python 3.5.1
+
+import sys
+import csv
+
+class CsvToTex():
+    def __init__(self):
+        self.begin = "\documentclass{jarticle}\n\n"+ \
+                      "\\begin{document}\n\n"+ \
+                      "\\begin{table}[htbp]\n"+ \
+                      "\t\caption{}\n"+ \
+                      "\t\label{}\n"+ \
+                      "\t\centering\n"
+
+        self.end = "\t\end{tabular}\n"+ \
+                    "\end{table}\n\n"+ \
+                    "\end{document}"
+
+    def spchar_escape(self,txt):
+        if isinstance(txt,str) == True:
+            spchar_dict = str.maketrans({"#":"\#",
+                                         "%":"\%",
+                                         "$":"\$",
+                                         "&":"\&",
+                                         "_":"\_",
+                                         "{":"\{",
+                                         "}":"\}",
+                                         "<":"\\textless",
+                                         ">":"\\textgreater",
+                                         "^":"\\textasciicircum",
+                                         "~":"\\textasciitilde",
+                                         "\\":"\\textbackslash",
+                                         "|":"\\textbar"})
+            return txt.translate(spchar_dict)
+        else:
+            print("Type Error!!")
+
+    def convert(self,txt):
+        return self.spchar_escape(txt).replace(","," & ")
+
+    def r_and_w(self,filename):
+        with open("output.tex","w") as texfile:
+            texfile.write(self.begin)
+            with open(filename,"r") as csvfile:
+                reader = csv.reader(csvfile)
+                header = next(reader)
+
+                texfile.write("\t"+"\\begin{tabular}{|"+str("c|" * len(header))+"} \hline\n")
+                header = ','.join(header)
+                texfile.write("\t"+self.convert(header)+" \\\\ \hline\n")
+
+                for line in reader:
+                    line = ','.join(line)
+                    value = self.convert(line)
+                    texfile.write("\t"+value.rstrip()+" \\\\ \hline\n")
+            texfile.write(self.end)
+
+if __name__ == '__main__':
+    args = sys.argv
+    filename = args[1]
+    ctt = CsvToTex()
+    ctt.r_and_w(filename)
